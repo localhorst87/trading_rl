0c71b586b37ca60c4c9b3ecb26e93b9048bb0ed8
linux_localhorst87 <m.p.ahmann@gmx.de>
2019-09-05 23:00:37 +0200

    added the WeightedMaxMinOpeningRewarder as an implementation and moved some methods and properties into the abstract base class. Renamed the old Rewarder implementation to CumulativeWeightedOpeningRewarder

--- lib/Rewarder.py
+++ lib/Rewarder.py
@@ -17,6 +17,7 @@ class Rewarder(ABC):
         self.tradeType = None # "long" / "short"
         self.sampleOpen = None
         self.sampleClose = None
+        self.rewardFunctionMap = {"keep_observing":self._rewardKeepObserving, "open_long":self._rewardOpenLong, "open_short":self._rewardOpenShort, "keep_position":self._rewardKeepPosition, "close_position":self._rewardClosePosition}
 
     def reset(self):
         ''' resetting the properties. Use this method when starting a new episode '''
@@ -25,36 +26,6 @@ class Rewarder(ABC):
         self.tradeType = None
         self.sampleOpen = None
         self.sampleClose = None
-
-    @property
-    def rewardFunctionMap(self):
-        pass
-
-    @abstractmethod
-    def getReward(self, dataset, action):
-        pass
-
-class SeparatedNetsSingleReward(Rewarder):
-    '''
-    Rewarder for actions of separated networks (one network where to decide when to open
-    a position and one for deciding when to close a position) that rewards only the last
-    action for each net (opening and closing). All actions where we observe or keep the
-    position are rewarded with 0.
-    '''
-
-    def __init__(self):
-        super().__init__()
-        self.WEIGHT_FACTOR_FORW = 0.97
-        self.WEIGHT_FACTOR_BACK = 0.75
-        self._rewardFunctionMap = {"keep_observing":self._rewardKeepObserving, "open_long":self._rewardOpenLong, "open_short":self._rewardOpenShort, "keep_position":self._rewardKeepPosition, "close_position":self._rewardClosePosition}
-
-    @property
-    def rewardFunctionMap(self):
-        return self._rewardFunctionMap
-
-    @rewardFunctionMap.setter
-    def rewardFunctionMap(self, value):
-        pass
 
     def getReward(self, dataset, action):
         ''' returns the reward of the given action. The correct method for the
@@ -69,6 +40,139 @@ class SeparatedNetsSingleReward(Rewarder):
         reward = rewardFunctionHandle()
 
         return reward
+
+class WeightedMaxMinOpeningRewarder(Rewarder):
+    '''
+        Rewarder for actions of separated networks (one network where to decide when to open
+        a position and one for deciding when to close a position) that rewards only the last
+        action for each net (opening and closing). All actions where we observe or keep the
+        position are rewarded with 0.
+
+        This Rewarder provides rewards for training of an opening agent only.
+        The reward for opening a position is calculated via a time and scale weighted
+        maximum and minimum future price.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.SAMPLES_FORWARD_OBSERVE = 200
+
+    def _rewardKeepObserving(self):
+        ''' returns the reward for observing the market before opening a position '''
+
+        return 0
+
+    def _rewardOpenLong(self):
+        ''' returns the reward for opening a long position '''
+
+        self.tradeType = "long"
+        self.sampleOpen = self.dataset.getPosition()
+        weightedReturns = self._calcWeightedReturns()
+        reward = self._calcRewardFromWeightedReturns(weightedReturns)
+
+        return reward
+
+    def _rewardOpenShort(self):
+        ''' returns the reward for opening a long position '''
+
+        self.tradeType = "short"
+        self.sampleOpen = self.dataset.getPosition()
+        weightedReturns = self._calcWeightedReturns()
+        reward = self._calcRewardFromWeightedReturns(weightedReturns)
+
+        return reward
+
+    def _rewardKeepPosition(self):
+        pass
+
+    def _rewardClosePosition(self):
+        pass
+
+    def _calcWeightedReturns(self):
+        ''' calculates the time and scale weighted price change in percentages relative to the
+            open position. Important: First weighting with the time weights and as second with
+            the scale weights, not vice versa!
+
+            OUT                 (list)      time and scale weighted returns
+        '''
+
+        if self.tradeType == "long":
+            price = self.dataset.data["price_ask_close"]
+            typeFactor = 1
+        elif self.tradeType == "short":
+            price = self.dataset.data["price_bid_close"]
+            typeFactor = -1
+        else:
+            raise ValueError("no valid open position existing")
+
+        start = self.sampleOpen + 1
+        end = start + self.SAMPLES_FORWARD_OBSERVE
+        tradeReturns =  100 * typeFactor * (price[start:end] - price[start]) / price[start] # percentage
+
+        timeWeights = self._getTimeWeights( len(tradeReturns) )
+        timeWeightedReturns = tradeReturns * timeWeights
+        weightedReturns = [self._scaleWeightFunction(x) for x in timeWeightedReturns.values]
+
+        return weightedReturns
+
+    def _calcRewardFromWeightedReturns(self, weightedReturns):
+        ''' calculates the reward from the weighted returns using the values of the maximum
+            and minimum values of the weighted returns
+
+            IN      weightedReturns     (list)      time and scale weighted returns
+            OUT                         (float)     reward
+        '''
+
+        minValue = min(weightedReturns)
+        maxValue = max(weightedReturns)
+        minPosition = np.argmin(weightedReturns)
+        maxPosition = np.argmax(weightedReturns)
+
+        expectedValue = maxValue + minValue
+
+        return scaledSigmoid(expectedValue, min = -1, max = 1, scaleFactor = 0.85)
+
+    def _getTimeWeights(self, length):
+        ''' calculates time weights for the trade returns. The further away the sample from the
+            opening position the lower the weight factor
+
+            IN      length      (int)       length of the weightedReturns
+            OUT                 (list)
+        '''
+
+        timeWeight = [scaledSigmoid(x, scaleFactor = 0.75) for x in np.linspace(5, -5, self.SAMPLES_FORWARD_OBSERVE)]
+
+        return timeWeight[:length]
+
+    def _scaleWeightFunction(self, tradeReturn):
+        ''' calculates the scale weighted trade return of a trade return: Higher values are
+            rewarded with a gaining factor, lower values are damped.
+            The damping and gaining is stronger for negative returns than for positive returns.
+
+            IN      tradeReturn     (float)     trade return
+            OUT                     (float)     scale weighted return
+        '''
+
+        power = 1.4 if tradeReturn >= 0 else 1.8
+
+        return np.sign(tradeReturn) * abs(tradeReturn)**power
+
+class CumulativeWeightedOpeningRewarder(Rewarder):
+    '''
+    Rewarder for actions of separated networks (one network where to decide when to open
+    a position and one for deciding when to close a position) that rewards only the last
+    action for each net (opening and closing). All actions where we observe or keep the
+    position are rewarded with 0.
+
+    This Rewarder provides rewards for training of an opening agent only.
+    The reward for opening a position is calculated via cumulative weighted price
+    changes of future prices and weighted missed return.
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.WEIGHT_FACTOR_FORW = 0.99
+        self.WEIGHT_FACTOR_BACK = 0.75
 
     def _rewardKeepObserving(self):
         ''' returns the reward for observing the market before opening a position '''
@@ -100,29 +204,10 @@ class SeparatedNetsSingleReward(Rewarder):
         return scaledSigmoid(reward, min = -1, max = 1, scaleFactor = 13000)
 
     def _rewardKeepPosition(self):
-        ''' returns the reward for holding a position '''
-
-        return 0
+        pass
 
     def _rewardClosePosition(self):
-        ''' returns the reward closing a position '''
-
-        self.sampleClose = self.dataset.getPosition()
-
-        if self.tradeType == "long":
-            priceOpen = self.dataset.data["price_ask_close"][self.sampleOpen]
-            priceClose = self.dataset.data["price_bid_close"][self.sampleClose]
-            typeFactor = 1
-        elif self.tradeType == "short":
-            priceOpen = self.dataset.data["price_bid_close"][self.sampleOpen]
-            priceClose = self.dataset.data["price_ask_close"][self.sampleClose]
-            typeFactor = -1
-        else:
-            raise ValueError("no valid open position existing")
-
-        tradeReturn = typeFactor * (priceClose - priceOpen) / priceOpen
-
-        return scaledSigmoid(tradeReturn, min = -1, max = 1, scaleFactor = 115)
+        pass
 
     def _calcMissedReturn(self, weightFactor):
         ''' calculates the missed return When opening a position. The missed
